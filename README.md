615b1756d6e2610158575eabd480d83cab92371c
huanwuji <huanwuji1989>
2017-03-23 23:35:49 +0800

    the first commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # akka-stream-in-action
+scala, akka, akka-stream 异步高性能编程实战
+
+- 同步，异步，非阻塞
+- scala原生异步支持,future,promise
+- akka多线程机制
+- akka vs akka-stream
+- 流式理解与应用
+- akka-stream简介
+- akka-stream应用场景
+- akka-stream tcp编程
+- akka-http编程
+- akka-stream现有支持
+          
---
+++ src/main/scala/MultiThreadExample.scala
@@ -0,0 +1,44 @@
+import java.util.concurrent.{Callable, Executors}
+
+import scala.concurrent.Future
+
+/**
+  * Created by huanwuji on 2017/3/23.
+  */
+object MultiThreadExample extends App {
+
+  def syncMethod(): Unit = {
+    var count = 0
+    for (i ← 1 to 10) {
+      count += 1
+    }
+    println(count)
+  }
+
+  def asyncMethod(): Unit = {
+    val executor = Executors.newFixedThreadPool(2)
+    var count = 0
+    val future1 = executor.submit(new Callable[Int] {
+      override def call(): Int = {
+        var count1 = 0
+        for (i ← 1 to 10) {
+          count1 += 1
+        }
+        count1
+      }
+    })
+    count = future1.get //获取结果是阻塞的
+    println(count)
+  }
+
+  def asyncNonblockMethod(): Unit = {
+    val fu = Future {
+      var count1 = 0
+      for (i ← 1 to 10) {
+        count1 += 1
+      }
+      count1
+    }
+    fu.onSuccess { case count ⇒ println(count) } //通过函数式回调避免线程阻塞
+  }
+}
